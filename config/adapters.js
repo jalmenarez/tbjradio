1dfa3c2f57039e07808cc66f55c595aa0be4bf76
Jose Almenarez <jalmenarez@trabajando.com>
2014-07-28 18:15:23 -0400

    Se quita los scopes del config, ya que puede variar dependiendo lo que se quiera hacer con la aplicacion.

--- config/adapters.js
+++ config/adapters.js
@@ -42,7 +42,6 @@ module.exports.adapters = {
     client_id: "c4b3222976e34dd581d9bd71a37e7677",
     client_secret: "b6a298fad16a4cdfbc0a7b3bdab0e6d4",
     redirect_uri: "http://localhost:1337/spotify/callback",
-    scopes: [ "user-read-private", "user-read-email" ],
     stateKey: "spotify_auth_state",
     username: "jalmenarez"
   }
