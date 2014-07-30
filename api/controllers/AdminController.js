e42c6632eb6e8981e1d6810ae1f698f685675f03
Jose Almenarez <jose.almenarez@gmail.com>
2014-07-30 09:53:00 -0400

    Se agrega el controller para el admin.

---
+++ api/controllers/AdminController.js
@@ -0,0 +1,65 @@
+/**
+ * AdminController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+    
+  
+  /**
+   * Action blueprints:
+   *    `/admin/login`
+   */
+   login: function (req, res) {
+	if(req.query.type == 'spotify') {
+		 res.redirect('/spotify/authorize');
+	} else {
+		 //TODO otro tipo de login.
+	}
+  },
+
+
+  /**
+   * Action blueprints:
+   *    `/admin/index`
+   *    `/admin`
+   */
+   index: function (req, res) {
+    return res.view({
+    	title: 'TBJRADIO'
+    });
+  },
+  
+  /**
+   * Action blueprints:
+   *    `/admin/dashboard`
+   */
+   dashboard: function (req, res) {  
+	   return res.view({
+		   title: 'TBJRADIO :: Dasboard'
+       });
+  },
+
+
+
+
+  /**
+   * Overrides for the settings in `config/controllers.js`
+   * (specific to AdminController)
+   */
+  _config: {}
+
+  
+};
