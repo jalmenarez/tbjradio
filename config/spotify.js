c748c4c2069980fe75efb55698f6f633e8aa91ba
Jose Almenarez <jose.almenarez@gmail.com>
2014-07-31 13:12:41 -0400

    Se deja con la configuracion de produccion.

--- config/spotify.js
+++ config/spotify.js
@@ -1,7 +1,7 @@
 module.exports.spotify = {
 		client_id: "c4b3222976e34dd581d9bd71a37e7677",
 		client_secret: "b6a298fad16a4cdfbc0a7b3bdab0e6d4",
-		redirect_uri: "http://localhost:1337/spotify/callback",
+		redirect_uri: "http://tbjradio.jit.su/spotify/callback",		
 		stateKey: "spotify_auth_state",
 		access_token_key: "spotify_access_token",
 		refresh_token_key: "spotify_refresh_token"
