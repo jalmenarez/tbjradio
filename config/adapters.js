885730a75a854be070770ba4a3613d7b2db6121d
Jose Almenarez <jose.almenarez@gmail.com>
2014-07-31 11:30:39 -0400

    Se saca la parte de Spotify ya que no corresponde.

--- config/adapters.js
+++ config/adapters.js
@@ -22,14 +22,6 @@ module.exports.adapters = {
   // (data is preserved when the server shuts down)
   disk: {
     module: 'sails-disk'
-  },
-
-  // Spotify
-  spotify: {
-    client_id: "c4b3222976e34dd581d9bd71a37e7677",
-    client_secret: "b6a298fad16a4cdfbc0a7b3bdab0e6d4",
-    redirect_uri: "http://tbjradio.jit.su/spotify/callback",
-    stateKey: "spotify_auth_state"
   }
 
 };
