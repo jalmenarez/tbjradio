450c0bdf6e182bae54e342269c2317f23f4a6b38
Jose Almenarez <jose.almenarez@gmail.com>
2014-07-31 11:28:42 -0400

    Se quita la construcion del objeto de la api web para que se construya donde se necesita.

--- api/services/SpotifyService.js
+++ api/services/SpotifyService.js
@@ -1,19 +1,5 @@
 
-var SpotifyWebApi = require('spotify-web-api-node');
-
-var config = global.Hook.config.adapters.spotify;
-
-var webApi = new SpotifyWebApi({
-					clientId : config.client_id,
-					clientSecret : config.client_secret,
-					redirectUri : config.redirect_uri
-				});
-
 module.exports = {
-
-    config: config,
-
-    webApi: webApi,
 
     /**
      * Generates a random string containing numbers and letters
