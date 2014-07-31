ef7606483b1d9083d970d52dfa479d0707e90108
Jose Almenarez <jose.almenarez@gmail.com>
2014-07-31 11:31:03 -0400

    Se agregan las configuraciones para Spotify.

---
+++ config/spotify.js
@@ -0,0 +1,8 @@
+module.exports.spotify = {
+		client_id: "c4b3222976e34dd581d9bd71a37e7677",
+		client_secret: "b6a298fad16a4cdfbc0a7b3bdab0e6d4",
+		redirect_uri: "http://localhost:1337/spotify/callback",
+		stateKey: "spotify_auth_state",
+		access_token_key: "spotify_access_token",
+		refresh_token_key: "spotify_refresh_token"
+};
