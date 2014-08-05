8a7b97c5694f95399f7545144d6e826a8a92db04
Jose Almenarez <jose.almenarez@gmail.com>
2014-08-05 10:24:49 -0400

    Se agrega metodo para validar los tokens antes de hacer una peticion a la api de spotify.

--- api/services/SpotifyService.js
+++ api/services/SpotifyService.js
@@ -7,6 +7,7 @@ module.exports = {
      * @return {string} The generated string
      */
     generateRandomString: function(length) {
+    	sails.log.debug('SpotifyService :: generateRandomString');
         var text = '';
         var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -17,6 +18,7 @@ module.exports = {
     },
 
     isAccessTokenExpired: function(req) {
+    	sails.log.debug('SpotifyService :: isAccessTokenExpired');
         if (req.session.tokenExpirationEpoch){
             var timeInSeconds = Math.floor(req.session.tokenExpirationEpoch - new Date().getTime() / 1000);
             sails.log.debug('Retrieved token. It expires in ' + timeInSeconds + ' seconds!');
@@ -27,6 +29,32 @@ module.exports = {
             }
         }else {
             return true;
+        }
+    },
+    
+    validateTokens: function(req, webApi){
+    	sails.log.debug('SpotifyService :: validateTokens');
+    	var storedAccessToken = req.session ? req.session.access_token : null;
+        sails.log.debug('access_token: ' + storedAccessToken);
+        webApi.setAccessToken(storedAccessToken);
+        var storedRefreshToken = req.session ? req.session.refresh_token : null;
+        webApi.setRefreshToken(storedRefreshToken);
+        sails.log.debug('refresh_token: ' + storedRefreshToken);
+        if(storedAccessToken != null && storedRefreshToken != null){
+        if (SpotifyService.isAccessTokenExpired(req)) {
+            webApi.refreshAccessToken().then(function (data) {
+                req.session.tokenExpirationEpoch = (new Date().getTime() / 1000) + data['expires_in'];
+                sails.log.debug('Refreshed token. It now expires in ' + Math.floor(req.session.tokenExpirationEpoch - new Date().getTime() / 1000) + ' seconds!');
+                return true;
+            }, function (err) {
+                sails.log.error(err);
+                return false;
+            });
+        }else {
+        	return true;
+        }
+        }else {
+        	return false;
         }
     }
 
