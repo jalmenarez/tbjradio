6f33d23c5a8d0533c81ba6e23cf32b2c49a9d2e3
Jose Almenarez <jose.almenarez@gmail.com>
2014-08-06 10:30:00 -0400

    Se cambia de nombre al model de las playlist

--- api/models/Playlist.js
+++ api/models/Playlist.js
@@ -1,18 +1,38 @@
 /**
-* Playlist.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Playlist
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
 
   attributes: {
 
-      name: 'string',
-      createBy: 'integer',
-      spotifyPlaylistId: 'string'
+  	id: {
+         type: 'string',
+         primaryKey: true
+    },
+
+    name: 'string',
+    description: 'string',
+    external_urls: 'json',
+    images: 'json',
+    owner: 'json',
+    uri: 'string',
+    href: 'string',
+    
+    // Add a reference to SpotifyUser
+    spotifyUserId: {
+          type: 'string'
+    },
+    
+    // for tracks
+    spotifyPagingId: {
+    	  type: 'integer'
+    } 
 
   }
-};
 
+};
