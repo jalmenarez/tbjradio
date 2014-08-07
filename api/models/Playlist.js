3a2168ae30622b23379c9ca4dab04f0ac1167419
Jose Almenarez <jose.almenarez@gmail.com>
2014-08-07 16:57:54 -0400

    Se implementa async para procesar las playlist obtenidas desde Spotify.

--- api/models/Playlist.js
+++ api/models/Playlist.js
@@ -6,6 +6,8 @@
  * @docs		:: http://sailsjs.org/#!documentation/models
  */
 
+var async = require('async');
+
 module.exports = {
 
   attributes: {
@@ -28,6 +30,53 @@ module.exports = {
           type: 'string'
     }      
 
-  }
+  },
+
+  createOrUpdateAll: function(items, cb){
+  	sails.log.debug('Playlist :: createOrUpdateAll');
+  	async.each(items, function( item, callback) {
+  		Playlist.findOne({ id: item.id }).exec(function (err, playlist) {
+  			if (!err && !playlist) {
+  				// create
+  				Playlist.create({
+  					name: item.name,
+  					id: item.id
+  				}).exec(function (err, playlist) {
+  					if (!err) {
+  						sails.log.info('created playlist: '+JSON.stringify(playlist));
+  						callback();
+  					} else {
+  						sails.log.error('error creating playlist', err);
+  						callback(err);
+  					}
+  				});
+  			} else if (!err) {
+  				// update
+  				Playlist.update({ id: item.id },
+  						{
+  						 name: item.name
+  						}).exec(function (err, playlist) {
+  							if (!err) {
+  								sails.log.info('updated playlist: '+JSON.stringify(playlist));
+  								callback();
+  							} else {
+  								sails.log.error('error updating playlist', err);
+  								callback(err);
+  							}
+  						});
+  			} else {
+  				sails.log.error(err);
+  				callback(err);
+  			}
+  		}); 		
+  	}, function(err){
+  		if(err) {
+  			sails.log.error('An item failed to process');
+  		} else {
+  		    sails.log.debug('All items have been processed successfully');
+  		    cb();
+  		}
+  	});    	
+  } 
 
 };
