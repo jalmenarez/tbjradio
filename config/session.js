5c40bef9d7b3e7f4521327d7f10b084f5b01013e
Jose Almenarez <jose.almenarez@gmail.com>
2014-08-15 10:38:19 -0400

    Se modifica el nombre de la base de datos mongo

--- config/session.js
+++ config/session.js
@@ -70,7 +70,7 @@ module.exports.session = {
    adapter: 'mongo',
    host: 'localhost',
    port: 27017,
-   db: 'tbjradio',
+   db: 'tbjradiodb',
    collection: 'sessions',
 
   /***************************************************************************
