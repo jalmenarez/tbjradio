386c2c5c5f6c0d63abcaffc030c7d6a5b7f888f6
Jose Almenarez <jose.almenarez@gmail.com>
2014-08-15 13:45:19 -0400

    Se agrega el controller para el manejo de los tracks.

---
+++ api/controllers/TrackController.js
@@ -0,0 +1,77 @@
+/**
+ * TrackController
+ *
+ * @description :: Server-side logic for managing tracks
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+var SpotifyWebApi = require('spotify-web-api-node');
+
+module.exports = {
+
+    /**
+     * `TrackController.synchronize()`
+     */
+    synchronize: function (req, res) {
+        sails.log.info('/playlist/synchronize_tracks');
+        var playlist_id = req.query.playlist_id;
+        sails.log.debug('playlist_id: ' + playlist_id);
+        var playlist_owner_id = req.query.playlist_owner_id;
+        sails.log.debug('playlist_owner_id: ' + playlist_owner_id);
+        var page = req.query.page ? req.query.page : 1;
+        sails.log.debug('page: ' + playlist_owner_id);
+        var limit = 100;
+        var offset = ((page - 1) * limit);
+        var total = 0;
+        var webApi = new SpotifyWebApi(sails.config.spotify.credentials);
+        if (SpotifyService.validateTokens(req, webApi)) {
+            if (req.session.spotifyUser && req.session.spotifyUser.id) {
+                var options = {
+                    offset: offset,
+                    limit: limit
+                };
+                webApi.getPlaylistTracks(playlist_owner_id, playlist_id, options)
+                    .then(function (response) {
+                        total = response.total;
+                        sails.log.debug('total: ' + total);
+                        offset = response.offset;
+                        sails.log.debug('offset: ' + offset);
+                        var pages = Math.round((total / limit) + 1);
+                        Track.createOrUpdateAll(response.items, function (err) {
+                            if (err) {
+                                return res.json({
+                                    result: 'NOK',
+                                    error: err
+                                });
+                            } else {
+                                return res.json({
+                                    result: 'OK',
+                                    page: page,
+                                    pages: pages
+                                });
+                            }
+                        });
+                    }).catch(function (err) {
+                        sails.log.error(err);
+                        return res.json({
+                            result: 'NOK',
+                            message: err
+                        });
+                    });
+            } else {
+                sails.log.error('userSpotify.id is null');
+                return res.json({
+                    result: 'NOK',
+                    message: 'userSpotify.id is null'
+                });
+            }
+        } else {
+            var error = 'INVALID_TOKENS';
+            sails.log.error(error);
+            return res.json({
+                result: 'NOK',
+                error: error
+            });
+        }
+    }
+};
